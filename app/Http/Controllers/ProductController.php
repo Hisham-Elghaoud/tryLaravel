77aa7723461e4b77c589b648d2b3500a37bef2c4
Hisham Elghaoud <hishamelghaoud2@gmail.com>
2022-05-22 23:24:24 +0200

    -----

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::get();
+        return view('products', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:10',
+            'price' => 'required|numeric'
+        ]);
+        if ($validator->fails()) {
+            $error = $validator->messages();
+            return response()->json(['error' => true, 'errors' => $error], 200);
+        }
+        Product::create([
+            'name' => $request->name,
+            'price' => $request->price
+        ]);
+        return redirect('/products');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('edit_product', compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update($id, Request $request)
+    {
+        $product = Product::findOrFail($id);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:10',
+            'price' => 'required|numeric'
+        ]);
+        if ($validator->fails()) {
+            $error = $validator->messages();
+            return response()->json(['error' => true, 'errors' => $error], 200);
+        }
+        $product->update([
+            'name' => $request->name,
+            'price' => $request->price
+        ]);
+        return redirect('/products');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+        return redirect('/products');
+    }
+}
